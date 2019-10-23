db4243c99227a52bab95f93a019ed22e5a06c7df
cao yin <caoyin2011@163.com>
2019-10-23 21:23:16 +0800

    add finish the add buttlomets

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
+
 
 def run_game():
     #初始化游戏并创建一个对象
@@ -14,11 +16,15 @@ def run_game():
 
     bg_color = (230,230,230)
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        bullets.update()
+        gf.update_bullets(bullets)
+        print(len(bullets))
+        gf.update_screen(ai_settings,screen,ship,bullets)
         #screen.fill(ai_settings.bg_color)
         #ship.blitme()
 
