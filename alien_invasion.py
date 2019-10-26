7d7b1442e6d21f12164388bd9b15dac3aaff5ef0
cao yin <caoyin2011@163.com>
2019-10-26 12:16:41 +0800

    add aliens all numbner on it

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from alien import Alien
 
 
 def run_game():
@@ -17,6 +18,11 @@ def run_game():
     bg_color = (230,230,230)
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+    
+    gf.create_fleet(ai_settings, screen,ship, aliens)
+
+    #alien = Alien(ai_settings, screen)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
@@ -24,7 +30,7 @@ def run_game():
         bullets.update()
         gf.update_bullets(bullets)
         print(len(bullets))
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
         #screen.fill(ai_settings.bg_color)
         #ship.blitme()
 
